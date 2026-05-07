208e1b310b29e39c6161eea07b257ea35652398f
Ecarteo <ecarteocalderon2017@gmail.com>
2026-05-07 14:27:15 -0400

    lesson 1 completed. try 2

--- lesson-1/js/script.js
+++ lesson-1/js/script.js
@@ -23,17 +23,25 @@
 		*/
 
 		// STEP 2: Get the EM element and store as a variable, using const
-		
+		const teacherEm = document.querySelector('em');
+
 		// STEP 3: Assign a click event listener to the above element, and include a reference to the function to be invoked
-		
-		// STEP 4: Creat a function that waits for the above click event
-		
+		teacherEm.addEventListener('click', updateName);
+
+		// STEP 4: Create a function that waits for the above click event
+		function updateName() {
+
 			// STEP 4a: Display a prompt asking for input, and assign the input to a variable using let
-			
+			let newName = prompt('Please enter a new name:');
+
 			// STEP 4b: Grab the input from the variable and build a new string, then change the text inside the EM element, using the textContent property
-			
-		
-		// STEP 5: Comment out the addEventListener above, and replace it with an onclick attribute typed directly into the EM element in the HTML - note that this is common, but it is considered to be bad practice - separate your HTML and your JS
+			if (newName) {
+				teacherEm.textContent = newName;
+			}
+		}
+
+		// STEP 5: Comment out the addEventListener above, and replace it with an onclick attribute typed directly into the EM element in the HTML 
+		// Note: To do this, you would comment out line 34 and add onclick="updateName()" to the <em> tag in your HTML file.
 
 		// STEP 6: Move the above JavaScript into an external file and reference it with an empty SCRIPT element inside the HEAD element - be sure to add a defer attribute in the SCRIPT start tag
 		// This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript
