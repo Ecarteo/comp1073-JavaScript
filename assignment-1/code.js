a6428b6f15159fd0ba054cb380c308b77fa5da27
Ecarteo <ecarteocalderon2017@gmail.com>
2026-06-12 22:31:54 -0400

    added logic for buttons listening

---
+++ assignment-1/code.js
@@ -0,0 +1,32 @@
+// Global variables
+const buttons = document.querySelectorAll('.toy-choice-buttons button');
+
+// Arrays
+const group1 = [""]
+const group2 = [""]
+const group3 = [""]
+const group4 = [""]
+const group5 = [""]
+const choices = [group1, group2, group3, group4, group5];
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        switch(index) {
+            case 0:
+                
+                break;
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+        }
+    });
+});
