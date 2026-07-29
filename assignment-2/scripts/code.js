94770398ad07343b92d6b09c048a7789379b155c
Ecarteo <ecarteocalderon2017@gmail.com>
2026-07-28 22:52:46 -0400

    chore: add student info the page using js

---
+++ assignment-2/scripts/code.js
@@ -0,0 +1,5 @@
+const studentInfoDiv = document.getElementById('student-info');
+const studentPara = document.createElement('p');
+studentPara.textContent = 'Student ID: 200630893 | Mateo Calderon Arce';
+studentInfoDiv.appendChild(studentPara);
+
