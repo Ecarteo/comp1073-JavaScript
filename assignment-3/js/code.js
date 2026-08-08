8efc714320a868a8d3f84b6d9232b23771f6725e
Ecarteo <ecarteocalderon2017@gmail.com>
2026-08-08 05:34:32 -0400

    feat: structure html and dynamically set student info

---
+++ assignment-3/js/code.js
@@ -0,0 +1,5 @@
+// load footer info
+const studentInfo = "Mateo Calderon Arce - 200630893";
+
+document.getElementById("current-year").textContent = new Date().getFullYear();
+document.getElementById("student-info").textContent = studentInfo;
