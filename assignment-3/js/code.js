e855537561bee0cd0b570283ef57aed9974c3dc1
Ecarteo <ecarteocalderon2017@gmail.com>
2026-08-09 01:18:33 -0400

    feat: start api fetch system

--- assignment-3/js/code.js
+++ assignment-3/js/code.js
@@ -3,3 +3,27 @@ const studentInfo = "Mateo Calderon Arce - 200630893";
 
 document.getElementById("current-year").textContent = new Date().getFullYear();
 document.getElementById("student-info").textContent = studentInfo;
+
+// jikan.api calls
+
+// fetch top 200 popular anime
+async function fetchPopularAnime() {
+    let topAnimeList = [];
+
+    // fetch pages 1 to 8 (25 items per page = 200 total)
+    for (let page = 1; page <= 8; page++) {
+        try {
+
+
+            
+        } catch (error) {
+            // catch network errors
+            console.error(`Page ${page} threw an error:`, error);
+        }
+
+        // pause briefly between calls to not hit Jikan's rate limit
+        await new Promise(resolve => setTimeout(resolve, 400));
+    }
+
+    return topAnimeList
+}
