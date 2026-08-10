31bb624104d458c58dc1546b79b60e57f501c311
Ecarteo <ecarteocalderon2017@gmail.com>
2026-08-10 02:07:02 -0400

    feat: add new logic, fetch seasonal anime instead

--- assignment-3/js/code.js
+++ assignment-3/js/code.js
@@ -4,4 +4,19 @@ const studentInfo = "Mateo Calderon Arce - 200630893";
 document.getElementById("current-year").textContent = new Date().getFullYear();
 document.getElementById("student-info").textContent = studentInfo;
 
-// 
+// fetch seasonal animes
+async function getSeasonalAnime() {
+    try {
+        const url = 'https://api.jikan.moe/v4/seasons/now'; // api call
+        const response = await fetch(url);
+
+        if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+        
+        const result = await response.json();
+        return result.data; // array of ~25 seasonal anime
+    } catch (error) {
+        console.error("Failed to fetch seasonal anime:", error);
+    }
+}
+
+getSeasonalAnime();
